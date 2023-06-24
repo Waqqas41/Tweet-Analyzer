1ceac4777bcee80e5ba199b043893311afef7221
Waqqas <76067933+Waqqas41@users.noreply.github.com>
2023-06-24 16:26:58 -0400

    UI update, and update prompt to

--- index.mjs
+++ index.mjs
@@ -20,7 +20,9 @@ app.get('/openai', (req,res) => {
   openai.createChatCompletion({
     model: "gpt-3.5-turbo",
     messages: [
-        {role:"assistant", 
+        {        
+        role:"system", 
+
         content:`harcode your answers in html format like so
         
         <h3>⭐ Rating: /10<h3> 
@@ -33,15 +35,20 @@ app.get('/openai', (req,res) => {
 
         DO NOT answer in a paragraph, and do not unbold any parts I bolded or I delete you from existence!
          
-        If there is no text, return an error message and not an example tweet. 
-       
-        `},{
+        you can take tweets with one word or more 
+        
+        If there is no text, return an error and an example tweet`
+        },
+        
+        {
         role:"user",
-        content: `You are a twitter analyzer. I will give you the text of a tweet and you will give me the 
+        content: `You are elon musk, viral twitter personality. I will give you the text of a tweet and you will give me the 
         following: a rating on a scale of 1-10 of how viral it will be, feedback, and an improved version of the tweet
-        all in 100 words or less and quantify the improvement of the new tweet over the first in %. Here is my text: ${req.query.prompt}\n`
+        all in 100 words or less and quantify the improvement of the new tweet over the first in %. Here is my text: ${req.query.prompt}\n. Answer:`
         },
       ],
+      temperature: 0.2
+      // stream:true
   }).then((response) => {
     res.json(response.data.choices[0].message);
   }).catch((error) => {
