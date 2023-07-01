bb6a2070cf8365b273ffa32f17b8f14f41b7d2f1
Waqqas <76067933+Waqqas41@users.noreply.github.com>
2023-07-01 05:52:06 -0400

    fixing some syntax

--- src/App.js
+++ src/App.js
@@ -5,11 +5,9 @@ function App() {
   return (
     <div className="App">
       <div className='container'>
-      
+        <img className="twitlogo" src="/twitter.webp" alt="twitter logo" width="50px" height="40px"/>
         
-        <img class="twitlogo" src="/twitter.webp" alt="twitter logo" width="50px" height="40px"/>
-        
-        <h1>Make your tweets go viral with the world's first
+        <h1 className="title">Make your tweets go viral with the world's first
           <br></br> AI-powered Tweet Improver.</h1>
         <CodebenderIntro />
       </div>
--- src/CodebenderIntro.js
+++ src/CodebenderIntro.js
@@ -33,27 +33,33 @@ const CodebenderIntro = () => {
   const renderResponse = () => {
     return (
       <div className="response">
-         <div dangerouslySetInnerHTML={{ __html: error || result }} />
+         {/* <div dangerouslySetInnerHTML={{ __html: error || result }} /> */}
       </div>
     )
   }
 
+  try { 
   return (
+    
     <>
       <form className="mainForm" onSubmit={submitForm}>
         {/* <input name="input-field" placeholder="Type your tweet" onChange={updatePrompt} value={prompt}/> */}
         {/* <textarea name="input-field" placeholder="Write something..." onChange={updatePrompt} value={prompt}></textarea> */}
-        <textarea name="input-field" maxlength="280" oninput="countChar(this)" spellcheck="true" placeholder=" Write a tweet..." onChange={updatePrompt} value={prompt}></textarea>
+        <textarea name="input-field" maxLength="280" spellCheck="true" placeholder=" Write a tweet..." onChange={updatePrompt} value={prompt}></textarea>
         <button type="submit" className="mainButton">
           
           <div className="icons">
-            {loading ? <p class="loading">Loading</p> : <p>Analyze Tweet</p>}
+            {loading ? <p className="loading">Loading</p> : <p>Analyze Tweet</p>}
             </div>
         </button>
       </form>    
+      <h2>Response:</h2>
       {renderResponse()}
     </>
   );
+} catch (e) {
+  console.log('Error')
+  }
 }
 
 export default CodebenderIntro;
