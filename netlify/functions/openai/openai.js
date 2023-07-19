66e312d76815182544a8ad0d3db648aa2618cc45
Waqqas <76067933+Waqqas41@users.noreply.github.com>
2023-07-19 17:04:01 -0400

    added mobile responsiveness

--- netlify/functions/openai/openai.js
+++ netlify/functions/openai/openai.js
@@ -23,30 +23,39 @@ exports.handler = async (event, context) => {
         {        
         role:"assistant", 
 
-        content:`hardcode your answers in html format like so. each point should be no longer than 2 lines
-        
-        <h3>⭐ Rating: /10</h3> 
-
-        <h3>✅ Feedback</h3> <p><b>reply</b></p>
-        
-        <h3>🤖 Improved Tweet</h3> <p><b>the tweet</b> comments</p>
-
-        answer in a consistent format. if a tweet doesn't make sense autocomplete it and answer it. never put hastags in your answers as they're bad
-         
-        you can analyze tweets with one word or more, even if it's something nonsensical like "ok" or "potato". 
-
-        make an html table of your answer and return it
-
-        if the tweet doesnt make sense give it a lower rating
-
+        content:`
        
         `
         }, 
         {
         role:"user",
-        content: `You are a professional tweet analyzer. you analyze even the most esoteric colloqual language. I will give you the text of a tweet and you will give me the 
-        following: a rating on a scale of 1-10 of how viral it will be, feedback, and an improved version of the tweet
-        all in 100 words or less and quantify the improvement of the new tweet over the first in %. do not include any hashtags in your answer. Here is my text: ${prompt}\n. Answer:`
+        content: `
+        hardcode your answers in html format like so. each point should be no longer than 2 lines
+        
+        <h3>⭐ Rating: /10</h3> 
+
+        <h3>✅ Feedback</h3> <p><b>reply</b></p>
+        
+        <h3>🤖 Improved Tweet</h3> <p><b>the tweet</b></p>
+
+        answer in a consistent format. if a tweet doesn't make sense autocomplete it and answer it. 
+        
+        never put hastags in your answers
+         
+        you can analyze tweets with one word or more, even if it's something nonsensical like "ok" or "potato". 
+
+        make an horizontal html table of your answer and return it
+
+        if the tweet doesnt make sense give it a lower rating
+
+        You are a professional tweet analyzer. 
+        never put hastags in your answers
+        you analyze even the most esoteric colloqual language. 
+        I will give you the text of a tweet and you will give me the 
+        following: a rating on a scale of 1-10 of how viral it will be, 
+        feedback, and an improved version of the tweet
+        all in 100 words or less and quantify the improvement of the new tweet over the first in %. 
+        do not include any hashtags in your answer. Here is my text: ${prompt}\n. Answer:`
            }],
     });
 
